30522feef04a4af144d101f89921566626fbccef
ErikBoesen <erik.boesen@gmail.com>
2015-06-23 20:05:43 -0400

    Removed more backgrounds and theme 5.

--- script.js
+++ script.js
@@ -5,6 +5,6 @@ addEventListener('load', function () {
     if (m < 10) {m = "0" + m;}
     document.getElementById("hours").innerHTML = h;
     document.getElementById("minutes").innerHTML = m;
-    document.body.style.backgroundImage = "url(bg/" + (Math.floor(Math.random() * 27) + 1) + ".jpg)";
-    document.getElementById("theme").href = "themes/" + (Math.floor(Math.random() * 5) + 1) + ".css";
+    document.body.style.backgroundImage = "url(bg/" + (Math.floor(Math.random() * 19) + 1) + ".jpg)";
+    document.getElementById("theme").href = "themes/" + (Math.floor(Math.random() * 4) + 1) + ".css";
 });
